d3d5875fa496e86fd986d6ceab3e9662481000e4
Tal Gold <talpogorelis@gmail.com>
2021-02-03 12:32:29 +0200

    added responsive search bar - design

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,16 @@ import React from "react";
 export const Navbar = () => {
   return (
     <div>
-      <div className="absolute flex items-center">
-        <label className="relative left-10">Search for a story!</label>
-        <input className="h-8 w-full rounded-xl border border-transparent focus:outline-none focus:ring focus:border-blue-300 p-3"></input>
+      <div className="relative flex items-center">
+        <input
+          className="h-12 w-full rounded-xl border border-transparent focus:outline-none focus:ring focus:border-blue-300 p-3"
+          placeholder="🔍 Search for a story! "
+          type="search"
+          name="serch"
+        ></input>
       </div>
     </div>
   );
 };
+
+//class="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none"
